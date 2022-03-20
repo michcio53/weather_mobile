d54028c19988ba70e73c5aa9b6e121ac9dc7cba1
Michał Sokół <michals1113@gmail.com>
2022-03-20 21:01:04 +0100

    feat: add GetLocationsByQueryUseCase in domain

---
+++ packages/domain/lib/usecase/get_locations_by_query_use_case.dart
@@ -0,0 +1,22 @@
+import 'package:domain/data_source/weather_data_source.dart';
+import 'package:domain/model/location.dart';
+import 'package:domain/usecase/use_case.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:fpdart/src/task_either.dart';
+
+enum GetLocationsByQueryUseCaseFailure { unexpected, empty }
+
+class GetLocationsByQueryUseCase extends ParamUseCase<GetLocationsByQueryUseCaseFailure, List<Location>, String> {
+  GetLocationsByQueryUseCase({
+    required WeatherDataSource weatherDataSource,
+  }) : _weatherDataSource = weatherDataSource;
+
+  final WeatherDataSource _weatherDataSource;
+
+  @override
+  TaskEither<GetLocationsByQueryUseCaseFailure, List<Location>> execute({required String param}) {
+    return _weatherDataSource
+        .getLocationByQuery(param)
+        .bimap((_) => GetLocationsByQueryUseCaseFailure.unexpected, (result) => result);
+  }
+}
---
+++ packages/domain/test/use_case/get_locations_by_query_use_case_test.dart
@@ -0,0 +1,56 @@
+import 'package:domain/data_source/weather_data_source.dart';
+import 'package:domain/model/error_detail.dart';
+import 'package:domain/model/location.dart';
+import 'package:domain/usecase/get_locations_by_query_use_case.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class MockWeatherDataSource extends Mock implements WeatherDataSource {}
+
+void main() {
+  late GetLocationsByQueryUseCase getLocationsByQueryUseCase;
+  late MockWeatherDataSource mockWeatherDataSource;
+
+  setUp(() {
+    mockWeatherDataSource = MockWeatherDataSource();
+    getLocationsByQueryUseCase = GetLocationsByQueryUseCase(weatherDataSource: mockWeatherDataSource);
+  });
+
+  test('Given correct parameters it invokes every method in usecase and returns locations list', () async {
+    when(() => mockWeatherDataSource.getLocationByQuery(any())).thenAnswer(
+      (_) => TaskEither.right(
+        [location],
+      ),
+    );
+
+    final result = await getLocationsByQueryUseCase.execute(param: 'param').run();
+
+    verify(
+      () => mockWeatherDataSource.getLocationByQuery(any()),
+    ).called(1);
+
+    result.fold((l) => throw Exception(l.toString()), (r) => expect(r, [location]));
+  });
+
+  test('Given ErrorDetailFatal to WeatherDataSource it returns GetWeatherForSavedLocationFailure.unexpected and invokes every method in use case', () async {
+    when(() => mockWeatherDataSource.getLocationByQuery(any())).thenAnswer(
+      (_) => TaskEither.left(ErrorDetailFatal()),
+    );
+
+    final result = await getLocationsByQueryUseCase.execute(param: 'param').run();
+
+    verify(
+      () => mockWeatherDataSource.getLocationByQuery(any()),
+    ).called(1);
+
+    result.fold((exceptionDetail) => expect(exceptionDetail, GetLocationsByQueryUseCaseFailure.unexpected), (r) {});
+  });
+}
+
+const location = Location(
+  title: 'title',
+  locationType: 'locationType',
+  lattLong: 'lattLong',
+  woeid: 123,
+);
