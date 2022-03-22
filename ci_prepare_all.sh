bde80e356d3930ba1184956bdd7c36438235ceb9
Michał Sokół <michals1113@gmail.com>
2022-03-22 19:54:07 +0100

    chore: update ci workflow

---
+++ ci_prepare_all.sh
@@ -0,0 +1,7 @@
+very_good packages get --recursive
+cd packages/domain/
+very_good packages get --recursive
+cd ../
+cd remote/
+very_good packages get --recursive
+cd ../../
---
+++ ci_test_all.sh
@@ -0,0 +1,7 @@
+flutter test --no-pub --coverage --test-randomize-ordering-seed random
+cd packages/domain/
+flutter test --no-pub --coverage --test-randomize-ordering-seed random
+cd ../
+cd remote/
+flutter test --no-pub --coverage --test-randomize-ordering-seed random
+cd ../../
