a80b6fd9ed6b924cbb67adf9ee7510b002313fa6
Michał Sokół <michals1113@gmail.com>
2022-03-25 07:54:30 +0100

    test: add navigation tests

--- test/screens/search/widget/search_body_success_test.dart
+++ test/screens/search/widget/search_body_success_test.dart
@@ -1,11 +1,18 @@
 import 'package:domain/model/location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockingjay/mockingjay.dart';
 import 'package:weather_mobile/screens/search/widget/search_body_success.dart';
 
 import '../../../helpers/helpers.dart';
 
 void main() {
+  late MockNavigator mockNavigator;
+
+  setUp(() {
+    mockNavigator = MockNavigator();
+  });
+
   testWidgets(
     'renders SearchBodySuccess with correct childrens',
     (tester) async {
@@ -22,6 +29,26 @@ void main() {
       final listViewWidget = tester.firstWidget<ListView>(find.byType(ListView));
 
       expect(listViewWidget.childrenDelegate.estimatedChildCount, locations.length);
+    },
+  );
+
+  testWidgets(
+    'when click on ListTile it pops page with location woeid',
+    (tester) async {
+      await tester.pumpApp(
+        Material(
+          child: MockNavigatorProvider(
+            navigator: mockNavigator,
+            child: SearchBodySuccess(
+              locations: locations,
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(ListTile));
+
+      verify(() => mockNavigator.pop(123)).called(1);
     },
   );
 }
--- test/screens/weather/widget/weather_page_bottom_navigation_bar_test.dart
+++ test/screens/weather/widget/weather_page_bottom_navigation_bar_test.dart
@@ -2,7 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockingjay/mockingjay.dart';
 import 'package:weather_mobile/screens/weather/bloc/weather_bloc.dart';
 import 'package:weather_mobile/screens/weather/widget/weather_page_bottom_navigation_bar.dart';
 
@@ -14,9 +14,11 @@ class FakeWeatherStarted extends Fake implements WeatherStarted {}
 
 void main() {
   late MockWeatherBloc mockWeatherBloc;
+  late MockNavigator mockNavigator;
 
   setUp(() {
     mockWeatherBloc = MockWeatherBloc();
+    mockNavigator = MockNavigator();
     registerFallbackValue(FakeWeatherStarted());
   });
 
@@ -125,6 +127,80 @@ void main() {
       final button = tester.firstWidget<IconButton>(find.byType(IconButton));
 
       expect(button.onPressed, isA<Function>());
+    });
+
+    testWidgets('when click on IconButton it pushes search page', (tester) async {
+      when(() => mockWeatherBloc.state).thenReturn(
+        const WeatherState.initial().copyWith(weatherStatus: WeatherStatus.success),
+      );
+      //ignore: body_might_complete_normally_nullable
+      when(() => mockNavigator.pushNamed(any())).thenAnswer((_) async {});
+
+      await tester.pumpApp(
+        BlocProvider<WeatherBloc>(
+          create: (_) => mockWeatherBloc,
+          child: MockNavigatorProvider(
+            navigator: mockNavigator,
+            child: const Material(
+              child: SearchIconButton(),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(IconButton));
+
+      verify(() => mockNavigator.pushNamed('/search')).called(1);
+    });
+
+    testWidgets('when return from previous screen with correct value it pushes event', (tester) async {
+      when(() => mockWeatherBloc.state).thenReturn(
+        const WeatherState.initial().copyWith(weatherStatus: WeatherStatus.success),
+      );
+      //ignore: body_might_complete_normally_nullable
+      when(() => mockNavigator.pushNamed(any())).thenAnswer((_) async => 123);
+      when(() => mockWeatherBloc.add(any())).thenAnswer((_) async {});
+
+      await tester.pumpApp(
+        BlocProvider<WeatherBloc>(
+          create: (_) => mockWeatherBloc,
+          child: MockNavigatorProvider(
+            navigator: mockNavigator,
+            child: const Material(
+              child: SearchIconButton(),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(IconButton));
+
+      verify(() => mockWeatherBloc.add(any())).called(1);
+    });
+
+    testWidgets('when return from previous screen with incorrect value it do nothing', (tester) async {
+      when(() => mockWeatherBloc.state).thenReturn(
+        const WeatherState.initial().copyWith(weatherStatus: WeatherStatus.success),
+      );
+      //ignore: body_might_complete_normally_nullable
+      when(() => mockNavigator.pushNamed(any())).thenAnswer((_) async => '123');
+      when(() => mockWeatherBloc.add(any())).thenAnswer((_) async {});
+
+      await tester.pumpApp(
+        BlocProvider<WeatherBloc>(
+          create: (_) => mockWeatherBloc,
+          child: MockNavigatorProvider(
+            navigator: mockNavigator,
+            child: const Material(
+              child: SearchIconButton(),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byType(IconButton));
+
+      verifyNever(() => mockWeatherBloc.add(any()));
     });
   });
 }
